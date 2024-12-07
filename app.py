0a90fcb7cdba32ac763217438720d763aa0783b3
WhisperedShadow <dd9514371@gmail.com>
2024-12-07 21:55:24 +0530

    the port is changed to default

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def index():
         return render_template('index.html', data=data, subdata=subser, cloud=cloud, highlights=highlight)
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
